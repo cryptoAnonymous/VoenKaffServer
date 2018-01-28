8f87789169bc8571f1908d305867871052202ca0
W1nterFox <aleks23041996@gmail.com>
2018-01-28 23:16:12 +0400

    Название форм

--- VoenKaffServer/FormSettings.Designer.cs
+++ VoenKaffServer/FormSettings.Designer.cs
@@ -41,9 +41,10 @@
             // 
             // TestDirectory
             // 
-            this.TestDirectory.Location = new System.Drawing.Point(211, 41);
+            this.TestDirectory.Location = new System.Drawing.Point(158, 33);
+            this.TestDirectory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TestDirectory.Name = "TestDirectory";
-            this.TestDirectory.Size = new System.Drawing.Size(219, 22);
+            this.TestDirectory.Size = new System.Drawing.Size(165, 20);
             this.TestDirectory.TabIndex = 0;
             this.TestDirectory.Leave += new System.EventHandler(this.TestDirectory_Leave);
             // 
@@ -51,17 +52,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Location = new System.Drawing.Point(9, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 18);
+            this.label1.Size = new System.Drawing.Size(143, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Директория с тестами:";
             // 
             // Port
             // 
-            this.Port.Location = new System.Drawing.Point(211, 87);
+            this.Port.Location = new System.Drawing.Point(158, 71);
+            this.Port.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Port.Name = "Port";
-            this.Port.Size = new System.Drawing.Size(67, 22);
+            this.Port.Size = new System.Drawing.Size(51, 20);
             this.Port.TabIndex = 2;
             this.Port.Leave += new System.EventHandler(this.Port_Leave);
             // 
@@ -69,17 +72,19 @@
             // 
             this.labelPort.AutoSize = true;
             this.labelPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelPort.Location = new System.Drawing.Point(12, 91);
+            this.labelPort.Location = new System.Drawing.Point(9, 74);
+            this.labelPort.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelPort.Name = "labelPort";
-            this.labelPort.Size = new System.Drawing.Size(47, 18);
+            this.labelPort.Size = new System.Drawing.Size(40, 15);
             this.labelPort.TabIndex = 3;
             this.labelPort.Text = "Порт:";
             // 
             // Close
             // 
-            this.Close.Location = new System.Drawing.Point(187, 259);
+            this.Close.Location = new System.Drawing.Point(140, 210);
+            this.Close.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Close.Name = "Close";
-            this.Close.Size = new System.Drawing.Size(194, 57);
+            this.Close.Size = new System.Drawing.Size(146, 46);
             this.Close.TabIndex = 4;
             this.Close.Text = "Закрыть";
             this.Close.UseVisualStyleBackColor = true;
@@ -87,9 +92,10 @@
             // 
             // IpAddress
             // 
-            this.IpAddress.Location = new System.Drawing.Point(211, 133);
+            this.IpAddress.Location = new System.Drawing.Point(158, 108);
+            this.IpAddress.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.IpAddress.Name = "IpAddress";
-            this.IpAddress.Size = new System.Drawing.Size(219, 22);
+            this.IpAddress.Size = new System.Drawing.Size(165, 20);
             this.IpAddress.TabIndex = 5;
             this.IpAddress.Leave += new System.EventHandler(this.IpAddress_Leave);
             // 
@@ -103,17 +109,18 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(12, 137);
+            this.label2.Location = new System.Drawing.Point(9, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 18);
+            this.label2.Size = new System.Drawing.Size(58, 15);
             this.label2.TabIndex = 7;
             this.label2.Text = "IP Адрес:";
             // 
             // FormSettings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(549, 362);
+            this.ClientSize = new System.Drawing.Size(412, 294);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.IpAddress);
             this.Controls.Add(this.Close);
@@ -121,8 +128,9 @@
             this.Controls.Add(this.Port);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TestDirectory);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormSettings";
-            this.Text = "Settings";
+            this.Text = "Настройки";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormSettings_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
