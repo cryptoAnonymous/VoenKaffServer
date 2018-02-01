705da213f4a752ef2fd071027616bcf7cf82526e
W1nterFox <aleks23041996@gmail.com>
2018-02-02 00:37:25 +0400

    try catch

--- VoenKaffServer/WordSaver.cs
+++ VoenKaffServer/WordSaver.cs
@@ -16,99 +16,110 @@ namespace VoenKaffServer
 
         public void Export_Data_To_Word(DataGridView DGV, string filename)
         {
-            object missing = System.Reflection.Missing.Value;
-            if (DGV.Rows.Count != 0)
+            try
             {
-                int RowCount = DGV.Rows.Count;
-                int ColumnCount = DGV.Columns.Count;
-                Object[,] DataArray = new object[RowCount + 1, ColumnCount + 1];
-
-                //add rows
-                int r = 0;
-                for (int c = 0; c <= ColumnCount - 1; c++)
+                object missing = System.Reflection.Missing.Value;
+                if (DGV.Rows.Count != 0)
                 {
-                    for (r = 0; r <= RowCount - 1; r++)
-                    {
-                        DataArray[r, c] = DGV.Rows[r].Cells[c].Value;
-                    } //end row loop
-                } //end column loop
+                    int RowCount = DGV.Rows.Count;
+                    int ColumnCount = DGV.Columns.Count;
+                    Object[,] DataArray = new object[RowCount + 1, ColumnCount + 1];
+                    int r = 0;
 
-                Word.Document oDoc = new Word.Document();
+                    //add rows
 
-                //page orintation
-                oDoc.PageSetup.Orientation = Word.WdOrientation.wdOrientLandscape;
-
-
-                dynamic oRange = oDoc.Content.Application.Selection.Range;
-                string oTemp = "";
-                for (r = 0; r <= RowCount - 1; r++)
-                {
                     for (int c = 0; c <= ColumnCount - 1; c++)
                     {
-                        
-                        oTemp = oTemp + DataArray[r, c]?.ToString().Replace("\t","").Replace("\n","").Replace("\r","") + "\t";
+                        for (r = 0; r <= RowCount - 1; r++)
+                        {
+                            DataArray[r, c] = DGV.Rows[r].Cells[c].Value;
+                        } //end row loop
+                    } //end column loop
 
+
+
+
+
+                    Word.Document oDoc = new Word.Document();
+
+                    //page orintation
+                    oDoc.PageSetup.Orientation = Word.WdOrientation.wdOrientLandscape;
+
+
+                    dynamic oRange = oDoc.Content.Application.Selection.Range;
+                    string oTemp = "";
+                    for (r = 0; r <= RowCount - 1; r++)
+                    {
+                        for (int c = 0; c <= ColumnCount - 1; c++)
+                        {
+
+                            oTemp = oTemp + DataArray[r, c]?.ToString().Replace("\t", "").Replace("\n", "").Replace("\r", "") + "\t";
+
+                        }
                     }
+
+                    //table format
+                    oRange.Text = oTemp;
+
+                    object Separator = Word.WdTableFieldSeparator.wdSeparateByTabs;
+                    object ApplyBorders = true;
+                    object AutoFit = true;
+                    object AutoFitBehavior = Word.WdAutoFitBehavior.wdAutoFitContent;
+
+                    oRange.ConvertToTable(ref Separator, ref RowCount, ref ColumnCount,
+                                          Type.Missing, Type.Missing, ref ApplyBorders,
+                                          Type.Missing, Type.Missing, Type.Missing,
+                                          Type.Missing, Type.Missing, Type.Missing,
+                                          Type.Missing, ref AutoFit, ref AutoFitBehavior, Type.Missing);
+
+                    oRange.Select();
+
+                    oDoc.Application.Selection.Tables[1].Select();
+                    oDoc.Application.Selection.Tables[1].Rows.AllowBreakAcrossPages = 0;
+                    oDoc.Application.Selection.Tables[1].Rows.Alignment = 0;
+                    oDoc.Application.Selection.Tables[1].Rows[1].Select();
+                    oDoc.Application.Selection.InsertRowsAbove(1);
+                    oDoc.Application.Selection.Tables[1].Rows[1].Select();
+
+                    //header row style
+                    oDoc.Application.Selection.Tables[1].Rows[1].Range.Bold = 1;
+                    oDoc.Application.Selection.Tables[1].Rows[1].Range.Font.Name = "Tahoma";
+                    oDoc.Application.Selection.Tables[1].Rows[1].Range.Font.Size = 14;
+
+                    //add header row manually
+                    for (int c = 0; c <= ColumnCount - 1; c++)
+                    {
+                        oDoc.Application.Selection.Tables[1].Cell(1, c + 1).Range.Text = DGV.Columns[c].HeaderText;
+                    }
+
+                    //table style 
+                    //oDoc.Application.Selection.Tables[1].set_Style("Grid Table 4 - Accent 5");
+                    oDoc.Application.Selection.Tables[1].Rows[1].Select();
+                    oDoc.Application.Selection.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+                    //header text
+                    foreach (Word.Section section in oDoc.Application.ActiveDocument.Sections)
+                    {
+                        Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                        headerRange.Fields.Add(headerRange, Word.WdFieldType.wdFieldPage);
+                        headerRange.Text = "Результаты";
+                        headerRange.Font.Size = 16;
+                        headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    }
+
+                    //save the file
+                    oDoc.SaveAs2(filename);
+                    oDoc.Close(ref missing, ref missing, ref missing);
+                    //NASSIM LOUCHANI
+
+
                 }
-
-                //table format
-                oRange.Text = oTemp;
-
-                object Separator = Word.WdTableFieldSeparator.wdSeparateByTabs;
-                object ApplyBorders = true;
-                object AutoFit = true;
-                object AutoFitBehavior = Word.WdAutoFitBehavior.wdAutoFitContent;
-
-                oRange.ConvertToTable(ref Separator, ref RowCount, ref ColumnCount,
-                                      Type.Missing, Type.Missing, ref ApplyBorders,
-                                      Type.Missing, Type.Missing, Type.Missing,
-                                      Type.Missing, Type.Missing, Type.Missing,
-                                      Type.Missing, ref AutoFit, ref AutoFitBehavior, Type.Missing);
-
-                oRange.Select();
-
-                oDoc.Application.Selection.Tables[1].Select();
-                oDoc.Application.Selection.Tables[1].Rows.AllowBreakAcrossPages = 0;
-                oDoc.Application.Selection.Tables[1].Rows.Alignment = 0;
-                oDoc.Application.Selection.Tables[1].Rows[1].Select();
-                oDoc.Application.Selection.InsertRowsAbove(1);
-                oDoc.Application.Selection.Tables[1].Rows[1].Select();
-
-                //header row style
-                oDoc.Application.Selection.Tables[1].Rows[1].Range.Bold = 1;
-                oDoc.Application.Selection.Tables[1].Rows[1].Range.Font.Name = "Tahoma";
-                oDoc.Application.Selection.Tables[1].Rows[1].Range.Font.Size = 14;
-
-                //add header row manually
-                for (int c = 0; c <= ColumnCount - 1; c++)
-                {
-                    oDoc.Application.Selection.Tables[1].Cell(1, c + 1).Range.Text = DGV.Columns[c].HeaderText;
-                }
-
-                //table style 
-                //oDoc.Application.Selection.Tables[1].set_Style("Grid Table 4 - Accent 5");
-                oDoc.Application.Selection.Tables[1].Rows[1].Select();
-                oDoc.Application.Selection.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
-
-                //header text
-                foreach (Word.Section section in oDoc.Application.ActiveDocument.Sections)
-                {
-                    Word.Range headerRange = section.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
-                    headerRange.Fields.Add(headerRange, Word.WdFieldType.wdFieldPage);
-                    headerRange.Text = "Результаты";
-                    headerRange.Font.Size = 16;
-                    headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
-                }
-
-                //save the file
-                oDoc.SaveAs2(filename);
-                oDoc.Close(ref missing, ref missing, ref missing);
-                //NASSIM LOUCHANI
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
-
-        
 
     }
 }
